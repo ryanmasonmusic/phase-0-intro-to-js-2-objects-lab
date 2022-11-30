009f199b7904b15f4820e52f98f4ff6e78886726
Ryan Mason <ryanmasonmusic@gmail.com>
2022-11-29 21:02:57 -0500

    commit tree acc099f78f201bd2428b3c5eee9a5ddf0a060164

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: "Ryan",
+    address: "7207 Desoto",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee [key] = value;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee [key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    const newKey = {...key};
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
